af0848e8bd83e9f9ca23f5853c136ea6f29034b2
Yoav Haimovitch <yoav.haimovitch@personetics.com>
2025-11-17 10:57:49 +0200

    chore: cleanup

--- src/experiments/tune2.py
+++ src/experiments/tune2.py
@@ -1,28 +1,7 @@
 from hyper_tuner import HyperTuner
 from config import EmbModel
 
-# TODO grid search around best from tune1
-
-structured_grid_config = {
-    'exp_params': {
-        'learning_rate': [1e-3, 5e-4],
-        'batch_size': [128, 256]
-    },
-    'emb_params': {
-        'model_name': [EmbModel.ALBERT, EmbModel.MiniLM_L12, EmbModel.FINBERT],
-    },
-    'feat_proc_params': {
-        'use_cyclical_dates': [True, False],
-        'use_categorical_amount': [False],
-        'use_continuous_amount': [True, False],
-        'use_categorical_dates': [True, False]
-
-    },
-    'model_params': {
-        'mlp_hidden_layers': [[64], [128, 64]],
-        'dropout_rate': [0.25, 0.4]
-    }
-}
+# grid search around best from tune1
 
 structured_grid_config = {
     'exp_params': {
